b341a6278a5126d4cbc7be4cb241356652d0f781
Phemis <phemisz@gmail.com>
2019-11-20 15:10:51 +0300

    Class Injector
    Class Injection tool for .NET app.

---
+++ Class Injector/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Class_Injector
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(145, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "RհօԵɑѵ";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Courier New", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(113, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 42);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Class Injector\r\n     by\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AllowDrop = true;
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(120, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Drag Here";
+            // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(15, 79);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(350, 138);
+            this.panel1.TabIndex = 4;
+            this.panel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Panel1_DragDrop_1);
+            this.panel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Panel1_DragEnter_1);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.button2.FlatAppearance.BorderSize = 2;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.ForeColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(210, 223);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 37);
+            this.button2.TabIndex = 6;
+            this.button2.TabStop = false;
+            this.button2.Text = "Inject";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(15, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 37);
+            this.button1.TabIndex = 5;
+            this.button1.TabStop = false;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(6, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Not Loaded !";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(343, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(22, 21);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Tag = "";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(147, 266);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(86, 22);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = ".cctor";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(133, 290);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(126, 22);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "EntryPoint";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(382, 319);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Class Injector ~ RհօԵɑѵ";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ Class Injector/Form1.cs
@@ -0,0 +1,201 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using dnlib.DotNet.Writer;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using static PX_InjectHelper.Inject_Helper;
+
+namespace Class_Injector
+{
+    public partial class Form1 : Form
+    {
+
+        #region Variables
+        string directoryName = "";
+        string filePath = "";
+        static ModuleDefMD module = null;
+        public Thread thr;
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void AddAntiDump(ModuleDef module , string methodName)
+        {
+            ModuleDefMD typeModule = ModuleDefMD.Load(typeof(PX_AntiDump.AntiDump).Module);
+            MethodDef injectMethod;
+            injectMethod = null;
+            if(radioButton1.Checked == true)
+            {
+                injectMethod = module.GlobalType.FindOrCreateStaticConstructor();
+            }
+            if(radioButton2.Checked == true)
+            {
+                injectMethod = module.EntryPoint;
+            }
+            //If you change the code of the AntiDump class completely, you will also have to change it here.
+          
+            TypeDef typeDef = typeModule.ResolveTypeDef(MDToken.ToRID(typeof(PX_AntiDump.AntiDump).MetadataToken)); 
+            IEnumerable<IDnlibDef> members = InjectHelper.Inject(typeDef, module.GlobalType, module); 
+
+            MethodDef init = (MethodDef)members.Single(method => method.Name == methodName);
+
+            injectMethod.Body.Instructions.Insert(0, Instruction.Create(OpCodes.Call, init));
+
+            foreach (TypeDef type in module.Types)
+            {
+                if (type.IsGlobalModuleType || type.Name == "Resources" || type.Name == "Settings" || type.Name.Contains("Form"))
+                    continue;
+                foreach (MethodDef method in type.Methods)
+                {
+                    if (!method.HasBody) continue;
+                    if (method.IsConstructor)
+                    {
+                        method.Body.Instructions.Insert(0, Instruction.Create(OpCodes.Nop));
+                        method.Body.Instructions.Insert(0, Instruction.Create(OpCodes.Call, init));
+                    }
+                }
+            }
+
+            foreach (MethodDef md in module.GlobalType.Methods)
+            {
+                if (md.Name != ".ctor") continue;
+                module.GlobalType.Remove(md);
+                break;
+            }
+        }
+        
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (filePath != string.Empty && module != null)
+            {
+                thr = new Thread(new ThreadStart(CodeBlock));
+                thr.Start();
+            }
+        }
+        public void CodeBlock()
+        {
+            //Modify the AntiDump class to change the desired class to be injected.
+
+            AddAntiDump(module , "Initialize"); //Method Name and Module
+            SaveAssembly();
+            MessageBox.Show("Class Injected Successfully!", "Class Injector", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        #region Github
+        private void Label2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Rhotav");
+        }
+        #endregion
+
+
+        #region Select
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog open = new OpenFileDialog();
+                open.Filter = "Executable Files|*.exe|DLL Files |*.dll";
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    module = ModuleDefMD.Load(open.FileName);
+                    filePath = open.FileName;
+                    pictureBox1.BackColor = Color.Lime;
+                    label4.Text = "Loaded !";
+                    label4.ForeColor = Color.Lime;
+                }
+            }
+            catch (Exception ex)
+            {
+                filePath = "";
+                module = null;
+                MessageBox.Show(ex.Message, "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                pictureBox1.BackColor = Color.Red;
+                label4.Text = "Not Loaded !";
+                label4.ForeColor = Color.Lime;
+            }
+        }
+        #endregion
+
+
+        #region Save
+        static void SaveAssembly()
+        {
+            var writerOptions = new NativeModuleWriterOptions(module, null);
+            writerOptions.Logger = DummyLogger.NoThrowInstance;
+            writerOptions.MetaDataOptions.Flags = (MetaDataFlags.PreserveTypeRefRids | MetaDataFlags.PreserveTypeDefRids | MetaDataFlags.PreserveFieldRids | MetaDataFlags.PreserveMethodRids | MetaDataFlags.PreserveParamRids | MetaDataFlags.PreserveMemberRefRids | MetaDataFlags.PreserveStandAloneSigRids | MetaDataFlags.PreserveEventRids | MetaDataFlags.PreservePropertyRids | MetaDataFlags.PreserveTypeSpecRids | MetaDataFlags.PreserveMethodSpecRids | MetaDataFlags.PreserveStringsOffsets | MetaDataFlags.PreserveUSOffsets | MetaDataFlags.PreserveBlobOffsets | MetaDataFlags.PreserveAll | MetaDataFlags.AlwaysCreateGuidHeap | MetaDataFlags.PreserveExtraSignatureData | MetaDataFlags.KeepOldMaxStack);
+            module.NativeWrite(Path.GetDirectoryName(module.Location) + @"\" + Path.GetFileNameWithoutExtension(module.Location) + "_inj.exe", writerOptions);
+        }
+        #endregion
+
+
+        #region Drag
+
+        private void Panel1_DragDrop_1(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void Panel1_DragEnter_1(object sender, DragEventArgs e)
+        {
+            try
+            {
+                Array array = (Array)e.Data.GetData(DataFormats.FileDrop);
+                if (array != null)
+                {
+                    string text = array.GetValue(0).ToString();
+                    int num = text.LastIndexOf(".");
+                    if (num != -1)
+                    {
+                        string text2 = text.Substring(num);
+                        text2 = text2.ToLower();
+                        if (text2 == ".exe" || text2 == ".dll")
+                        {
+                            Activate();
+                            int num2 = text.LastIndexOf("\\");
+                            if (num2 != -1)
+                            {
+                                directoryName = text.Remove(num2, text.Length - num2);
+                            }
+                            if (directoryName.Length == 2)
+                            {
+                                directoryName += "\\";
+                            }
+                            module = ModuleDefMD.Load(text);
+                            filePath = text;
+                            pictureBox1.BackColor = Color.Lime;
+                            label4.Text = "Loaded !";
+                            label4.ForeColor = Color.Lime;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                filePath = "";
+                module = null;
+                MessageBox.Show(ex.Message, "Error !", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                pictureBox1.BackColor = Color.Red;
+                label4.Text = "Not Loaded !";
+                label4.ForeColor = Color.Lime;
+            }
+        }
+        #endregion
+    }
+}
